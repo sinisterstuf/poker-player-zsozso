e77c967c0d9234252abd71beb706db1ca67db4c0
Siôn Le Roux <sinisterstuf@gmail.com>
2015-03-21 10:25:09 +0100

    Tabs to 2-spaces lol; only whitespace changes

--- player.js
+++ player.js
@@ -1,11 +1,10 @@
-
 module.exports = {
 
   VERSION: "Super Zsozso team",
 
   bet_request: function(game_state) {
-	  return calculate_move(game_state);
-	  //var bet = 1100;
+    return calculate_move(game_state);
+    //var bet = 1100;
   },
 
   showdown: function(game_state) {
@@ -17,12 +16,12 @@ function calculate_move(game_state) {
   try {
     var bet = 110;
 
-	bet = game_state["current_buy_in"] - game_state["players"][game_state["in_action"]][game_state["bet"]] + game_state["minimum_raise"] + bet;
+    bet = game_state["current_buy_in"] - game_state["players"][game_state["in_action"]][game_state["bet"]] + game_state["minimum_raise"] + bet;
 
-	  if(isNaN(parseInt(bet))){
-		  bet = 1100;
-	  }
-	  console.log("DO BET: "+bet);
+    if(isNaN(parseInt(bet))){
+      bet = 1100;
+    }
+    console.log("DO BET: "+bet);
     return bet;
   } catch(e) {
     console.log('caught horrible exception; folding!');
