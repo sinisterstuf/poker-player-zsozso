5d432c1ccd526654cd5941e2f21101292a8a8061
Bence Berenyi <bence@ujweblap.com>
2015-03-21 10:04:44 +0100

    fix 2

--- player.js
+++ player.js
@@ -9,6 +9,9 @@ module.exports = {
       //bet = parseInt(game_state.minimum_raise) + bet;
 	bet = game_state.current_buy_in - game_state.players[game_state.in_action][game_state.bet] + game_state.minimum_raise + 55;
     //}
+	  if(isNaN(parseInt(bet))){
+		  bet = 500;
+	  }
     return bet;
   },
 
