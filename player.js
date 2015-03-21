923e779abce88de388b26056cfa53843289341a4
Bence Berenyi <bence@ujweblap.com>
2015-03-21 10:03:21 +0100

    fix bet

--- player.js
+++ player.js
@@ -7,7 +7,7 @@ module.exports = {
     var bet = 55;
     //if(game_state.minumum_raise) {
       //bet = parseInt(game_state.minimum_raise) + bet;
-	bet = game_state.current_buy_in - game_state.players[game_state.in_action][bet] + game_state.minimum_raise + 55;
+	bet = game_state.current_buy_in - game_state.players[game_state.in_action][game_state.bet] + game_state.minimum_raise + 55;
     //}
     return bet;
   },
