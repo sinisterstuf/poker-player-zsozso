1cb0755391487c09174c1a632248df7373d80ccd
Bence Berenyi <bence@ujweblap.com>
2015-03-21 10:22:01 +0100

    fix

--- player.js
+++ player.js
@@ -18,7 +18,7 @@ function calculate_move(game_state) {
     var bet = 110;
 
 	bet = game_state["current_buy_in"] - game_state["players"][game_state["in_action"]][game_state["bet"]] + game_state["minimum_raise"] + bet;
-    
+
 	  if(isNaN(parseInt(bet))){
 		  bet = 1100;
 	  }
