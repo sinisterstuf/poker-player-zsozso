9a9abcedca8875258eab6a410e1c81ba83bd39e6
Tibor Molnar <tibor.molnar@schibsted.hu>
2015-03-21 13:13:35 +0100

    Http logging

--- sandbox_gamestate.js
+++ sandbox_gamestate.js
@@ -2,5 +2,9 @@ var demo_state = require("./sample_game_state");
 
 console.log("POT:", demo_state["players"][demo_state["in_action"]]["hole_cards"][0]["rank"]);
 var logger = require('./logger/logger');
-logger.info('yoooohooooo');
+logger.log('Yoooo dawwwwggg');
 console.log("POT:", demo_state.pot);
+
+setTimeout(function() {
+
+}, 50000);
