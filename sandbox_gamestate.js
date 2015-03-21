8c5173db972b40825d92540d679f9cd0d93c6589
Tibor Molnar <tibor.molnar@schibsted.hu>
2015-03-21 11:48:48 +0100

    Test logging

--- sandbox_gamestate.js
+++ sandbox_gamestate.js
@@ -1,3 +1,6 @@
 var demo_state = require("./sample_game_state");
 
 console.log("POT:", demo_state["players"][demo_state["in_action"]]["hole_cards"][0]["rank"]);
+var logger = require('./logger/logger');
+logger.info('yoooohooooo');
+console.log("POT:", demo_state.pot);
