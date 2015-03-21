924ac42134f78dde85229cc65e81920cd3937322
Bence Berenyi <bence@ujweblap.com>
2015-03-21 12:42:07 +0100

    fix type ;)

--- test.js
+++ test.js
@@ -4,7 +4,11 @@ require('should');
 
 describe('calulateMove', function () {
   it('returns an int', function () {
-    var result = logic.calulateMove();
+    console.log("logic",typeof logic);
+    console.log("logic has calc",logic.hasOwnProperty("calculateMove"));
+    console.log("logic calc type",typeof logic.calculateMove);
+    console.log("logic calc type return type",typeof logic.calculateMove());
+    var result = logic.calculateMove(gs);
     result.should.eql(parseInt(result));
   });
 });
