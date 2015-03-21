11278ce5ee52eef8905732c67c0840101646bc1e
Siôn Le Roux <sinisterstuf@gmail.com>
2015-03-21 10:07:00 +0100

    Pull logic out into a try-catched function

--- player.js
+++ player.js
@@ -4,15 +4,24 @@ module.exports = {
   VERSION: "Default JavaScript folding player",
 
   bet_request: function(game_state) {
-    var bet = 55;
-    //if(game_state.minumum_raise) {
-      //bet = parseInt(game_state.minimum_raise) + bet;
-	bet = game_state.current_buy_in - game_state.players[game_state.in_action][bet] + game_state.minimum_raise + 55;
-    //}
-    return bet;
+    calculate_move(game_state);
   },
 
   showdown: function(game_state) {
 
   }
 };
+
+function calculate_move(game_state) {
+  try {
+    var bet = 55;
+    //if(game_state.minumum_raise) {
+      //bet = parseInt(game_state.minimum_raise) + bet;
+	bet = game_state.current_buy_in - game_state.players[game_state.in_action][bet] + game_state.minimum_raise + 55;
+    //}
+    return bet;
+  } catch(e) {
+    console.log('caught horrible exception; folding!');
+    return 0;
+  }
+}
