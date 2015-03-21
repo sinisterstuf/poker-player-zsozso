d817134e4196b05a6f05bd0d016a837d15d36494
Bence Berenyi <bence@ujweblap.com>
2015-03-21 10:00:18 +0100

    base bet

--- player.js
+++ player.js
@@ -5,9 +5,10 @@ module.exports = {
 
   bet_request: function(game_state) {
     var bet = 55;
-    if(game_state.minumum_raise) {
-      bet = parseInt(game_state.minimum_raise) + bet;
-    }
+    //if(game_state.minumum_raise) {
+      //bet = parseInt(game_state.minimum_raise) + bet;
+	bet = game_state.current_buy_in - game_state.players[game_state.in_action][bet] + game_state.minimum_raise + 55;
+    //}
     return bet;
   },
 
