06bda5d6dae7e8e9d72f5aa86a097ab23f51a47a
Bence Berenyi <bence@ujweblap.com>
2015-03-21 09:55:49 +0100

    raise bet , minimum + 55

--- player.js
+++ player.js
@@ -4,7 +4,11 @@ module.exports = {
   VERSION: "Default JavaScript folding player",
 
   bet_request: function(game_state) {
-    return 55;
+    var bet = 55;
+    if(game_state.minumum_raise) {
+      bet = parseInt(game_state.minimum_raise) + bet;
+    }
+    return bet;
   },
 
   showdown: function(game_state) {
