ead87e84f611d86ae99f8cbde878a3248a3d7584
Bence Berenyi <bence@ujweblap.com>
2015-03-21 10:10:53 +0100

    fix bet

--- player.js
+++ player.js
@@ -4,13 +4,14 @@ module.exports = {
   VERSION: "Default JavaScript folding player",
 
   bet_request: function(game_state) {
-    var bet = 55;
+    var bet = 1100;
     //if(game_state.minumum_raise) {
       //bet = parseInt(game_state.minimum_raise) + bet;
-	bet = game_state.current_buy_in - game_state.players[game_state.in_action][game_state.bet] + game_state.minimum_raise + 55;
+
+	bet = game_state["current_buy_in"] - game_state["players"][game_state["in_action"]][game_state["bet"]] + game_state["minimum_raise"] + bet;
     //}
 	  if(isNaN(parseInt(bet))){
-		  bet = 500;
+		  bet = 1100;
 	  }
     return bet;
   },
